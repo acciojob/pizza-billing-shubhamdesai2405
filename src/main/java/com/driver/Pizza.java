78ea018e7b71e8be711230be7345205089a9638d
Shubham <shubhamdesai2405@gmail.com>
2022-10-10 10:43:02 +0530

    I think final

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,26 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private boolean cheese=false;
+    private boolean topping =false;
+    private boolean bag=false;
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg){
+//            this.price = 300;
+            setPrice(300);
+        }else{
+//            this.price = 400;
+            setPrice(400);
+        }
+
         // your code goes here
+    }
+
+    public void setPrice(int price){
+        this.price += price;
     }
 
     public int getPrice(){
@@ -16,19 +33,62 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
+
+        if(!cheese){
+            this.cheese=true;
+            setPrice(80);
+        }
         // your code goes here
     }
 
     public void addExtraToppings(){
+
+        if(!topping){
+            this.topping=true;
+            int vegTopping = 70;
+            int nonVegTopping = 120;
+            if(isVeg){
+                setPrice(vegTopping);
+            }else{
+                setPrice(nonVegTopping);
+            }
+        }
         // your code goes here
     }
 
     public void addTakeaway(){
+
+       if(!bag){
+           this.bag=true;
+           setPrice(20);
+       }
         // your code goes here
     }
 
     public String getBill(){
+        String Bill="";
+        if(isVeg){
+            Bill += "Base Price Of The Pizza: " + "300" + "\n";
+        }else{
+            Bill += "Base Price Of The Pizza: " + "400" + "\n";
+        }
+        if(cheese){
+            Bill += "Extra Cheese Added: "+"80"+"\n";
+        }
+        if(topping){
+            if(isVeg){
+              Bill +=  "Extra Toppings Added: "+"70"+"\n";
+            }else{
+                Bill +=  "Extra Toppings Added: "+"120"+"\n";
+            }
+        }
+        if(bag){
+            Bill += "Paperbag Added: "+"20"+"\n";
+
+        }
+        Bill += "Total Price: "+""+this.price;
         // your code goes here
+        this.bill = Bill;
         return this.bill;
     }
 }
